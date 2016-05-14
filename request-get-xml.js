0e763fdf1884b511d1a21fd45dac35aa64805793
Danny Tizon <dtizon@gmail.com>
2016-05-13 21:09:20 -0700

    noted that key is public

--- request-get-xml.js
+++ request-get-xml.js
@@ -2,7 +2,7 @@ var request = require('request');
 var parseXml = require('xml2js').parseString;
 var util = require('util');
 
-//sample REST GET (XML response)
+//sample REST GET (XML response) - note key is a public key
 request.get('http://api.bart.gov/api/etd.aspx?cmd=etd&orig=dubl&key=MW9S-E7SL-26DU-VV8V', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log(body); // Show the response
